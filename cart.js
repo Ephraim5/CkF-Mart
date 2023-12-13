6d22d056b21bfa560ecb9cda2d24940804c82d9a
Ephraim5 <111990405+Ephraim5@users.noreply.github.com>
2023-12-13 22:48:23 +0100

    Add files via upload

--- cart.js
+++ cart.js
@@ -1,5 +1,6 @@
 var cart_btn = document.getElementById("cart_btn");
 var cart_b = document.getElementById("cart_b");
+var cart_bd = document.getElementById("cart_bd");
 var carts =document.querySelector(".carts");
 var side_bar=document.getElementById("sidebar");
 var buy =document.querySelectorAll(".buynow");
@@ -8,6 +9,7 @@ var counter =document.querySelectorAll("#count");
 
 
 cart_b.addEventListener("click",carted)
+cart_bd.addEventListener("click",carted)
 cart_btn.addEventListener("click",eme);
 function arrow(){
    carts.classList.remove("active");
@@ -25,56 +27,63 @@ function carted(){
 }
 
 var con=[];
+const username="";
 buy.forEach(bought=>{
    bought.addEventListener("click",(e)=>{
+      if(username==""){
+         alert("please register first before buying goods....")
+         return;
+      }else{
       e.preventDefault();
-  
-     var mom= bought.parentNode;
-     var brand_box = mom.childNodes[1];
-     var i = brand_box.childNodes[1];
-     var h2 = brand_box.childNodes[3];
-     var price=  brand_box.childNodes[5];
-     var img =i.childNodes[0];
-      // console.log(mom);
-//       <div class="carter">
-//       <img src="./icon/p2.png" alt="">
-//       <h1>Cloth </h1>
-//       <input type="number" placeholder="1"  id="count">
-//       <span>Price $10.98</span>
-//   </div>
-      var carter = document.createElement("div");
-         carter.classList.add("carter");
-         cart_room.appendChild(carter);
-      var pic =document.createElement("img");
-          pic.src=img.src;
-         carter.appendChild(pic);
-      var h =document.createElement("h2");
-         h.innerHTML=h2.textContent;
-         carter.appendChild(h);
-          
-      var c= document.createElement("input");
-          c.setAttribute("type","number")
-          c.setAttribute("id","count")
-          c.classList.add("input")
-          c.setAttribute("placeholder","X")
-          c.setAttribute("readonly","readonly")
-          c.setAttribute("onclick","lot(event)");
-          carter.appendChild(c);
-      var s= document.createElement("span");
-          s.innerHTML=price.innerText;
-          carter.appendChild(s);
-          con.push(s.innerHTML.replace("Price $",""))
-         
-      var total=0;
-      con.forEach(co=>{
-       total +=co;
-     })
-console.log(total)
-         
-      cart_room.scrollTo(0,cart_room.scrollHeight);
-      // console.log(go);
-   
-   })
+     
+        var mom= bought.parentNode;
+        var brand_box = mom.childNodes[1];
+        var i = brand_box.childNodes[1];
+        var h2 = brand_box.childNodes[3];
+        var price=  brand_box.childNodes[5];
+        var img =i.childNodes[0];
+         // console.log(mom);
+   //       <div class="carter">
+   //       <img src="./icon/p2.png" alt="">
+   //       <h1>Cloth </h1>
+   //       <input type="number" placeholder="1"  id="count">
+   //       <span>Price $10.98</span>
+   //   </div>
+         var carter = document.createElement("div");
+            carter.classList.add("carter");
+            cart_room.appendChild(carter);
+         var pic =document.createElement("img");
+             pic.src=img.src;
+            carter.appendChild(pic);
+         var h =document.createElement("h2");
+            h.innerHTML=h2.textContent;
+            carter.appendChild(h);
+             
+         var c= document.createElement("input");
+             c.setAttribute("type","number")
+             c.setAttribute("id","count")
+             c.classList.add("input")
+             c.setAttribute("placeholder","X")
+             c.setAttribute("readonly","readonly")
+             c.setAttribute("onclick","lot(event)");
+             carter.appendChild(c);
+         var s= document.createElement("span");
+             s.innerHTML=price.innerText;
+             carter.appendChild(s);
+             con.push(s.innerHTML.replace("Price $",""))
+            
+         var total=0;
+         con.forEach(co=>{
+          total +=co;
+        })
+         console.log(total)
+            
+         cart_room.scrollTo(0,cart_room.scrollHeight);
+         // console.log(go);
+      }
+        })
+      
+
   
 })
 function lot(event) {
